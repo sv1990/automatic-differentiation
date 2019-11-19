1b04578c7e9a697b8e9370e64d96446c556263e3
Semir Vrana <semir.vrana@gmx.de>
2019-11-19 18:56:17 +0100

    Initial commit

---
+++ main.cc
@@ -0,0 +1,128 @@
+#include <iomanip>
+#include <iostream>
+#include <type_traits>
+
+namespace ad {
+template <typename T>
+struct is_expr : std::false_type {};
+
+template <typename T>
+constexpr bool is_expr_v = is_expr<T>::value;
+
+// TODO: Add zero and unity as special structs to help with optimizations of 1*x
+// and x+0
+
+struct constant {
+  double value;
+  constexpr double operator()(double) const noexcept { return value; }
+  constexpr auto derive() const noexcept { return constant{0}; }
+};
+
+template <>
+struct is_expr<constant> : std::true_type {};
+
+template <char... cs>
+struct variable {
+  constexpr double operator()(double x) const noexcept { return x; }
+  constexpr auto derive() const noexcept { return constant{1}; }
+};
+
+template <char... cs>
+struct is_expr<variable<cs...>> : std::true_type {};
+
+namespace var {
+const variable<'x'> x;
+}
+
+template <typename L, typename R>
+struct addition;
+
+template <typename L, typename R>
+addition<L, R> make_addition(L l, R r) noexcept;
+
+template <typename L, typename R>
+struct addition {
+  L lhs;
+  R rhs;
+  constexpr addition(L lhs_, R rhs_) noexcept : lhs(lhs_), rhs(rhs_) {}
+  constexpr double operator()(double x) const noexcept {
+    return lhs(x) + rhs(x);
+  }
+  constexpr auto derive() const noexcept {
+    return make_addition(lhs.derive(), rhs.derive());
+  }
+};
+
+template <typename L, typename R>
+addition<L, R> make_addition(L l, R r) noexcept {
+  return addition<L, R>(l, r);
+}
+
+template <typename L, typename R>
+struct is_expr<addition<L, R>> : std::true_type {};
+
+template <typename L, typename R,
+          std::enable_if_t<is_expr_v<L> && is_expr_v<R>>* = nullptr>
+auto operator+(L l, R r) noexcept {
+  return addition<L, R>{l, r};
+}
+
+template <typename L, typename R>
+struct multiplication {
+  L lhs;
+  R rhs;
+  constexpr multiplication(L lhs_, R rhs_) noexcept : lhs(lhs_), rhs(rhs_) {}
+  constexpr double operator()(double x) const noexcept {
+    return lhs(x) * rhs(x);
+  }
+  constexpr auto derive() const noexcept {
+    return make_addition(make_multiplication(lhs.derive(), rhs),
+                         make_multiplication(lhs, rhs.derive()));
+  }
+};
+
+template <typename L, typename R>
+multiplication<L, R> make_multiplication(L l, R r) noexcept {
+  return multiplication<L, R>(l, r);
+}
+
+template <typename L, typename R>
+struct is_expr<multiplication<L, R>> : std::true_type {};
+
+template <typename L, typename R,
+          std::enable_if_t<is_expr_v<L> && is_expr_v<R>>* = nullptr>
+auto operator*(L l, R r) noexcept {
+  return multiplication<L, R>{l, r};
+}
+
+namespace literals {
+constexpr constant operator""_c(long double x) {
+  return constant{static_cast<double>(x)};
+}
+constexpr constant operator""_c(unsigned long long x) {
+  return constant{static_cast<double>(x)};
+}
+
+} // namespace literals
+} // namespace ad
+
+int main() {
+  using namespace ad::literals;
+  using ad::var::x;
+
+  const auto f   = x * (x + 1_c);
+  const auto df  = f.derive();
+  const auto d2f = df.derive();
+
+  const int colwidth = 7;
+  std::cout << std::setw(colwidth) << std::right << "x" << ' '     //
+            << std::setw(colwidth) << std::right << "f(x)" << ' '  //
+            << std::setw(colwidth) << std::right << "df(x)" << ' ' //
+            << std::setw(colwidth) << std::right << "d2f(x)" << '\n';
+  for (double x = 0; x <= 10; x += 1) {
+    std::cout << std::setw(colwidth) << std::right << x << ' '     //
+              << std::setw(colwidth) << std::right << f(x) << ' '  //
+              << std::setw(colwidth) << std::right << df(x) << ' ' //
+              << std::setw(colwidth) << std::right << d2f(x) << '\n';
+  }
+}
