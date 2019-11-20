4167777ba777a687ff07531196567e6357cf9f48
Semir Vrana <semir.vrana@gmx.de>
2019-11-20 13:44:32 +0100

    Improve multidimensional example

--- multi_dimensional.cc
+++ multi_dimensional.cc
@@ -7,14 +7,18 @@ int main() {
   const auto x = ad::_0;
   const auto y = ad::_1;
 
-  const auto f = x * y;
-
-  std::cout << f(1, 1) << '\n';
-  std::cout << f(1, 2) << '\n';
+  const auto f = x * x * y;
 
   const auto dxf = f.derive<0>();
   const auto dyf = f.derive<1>();
 
-  std::cout << dxf(1, 0) << ' ' << dxf(0, 1) << '\n';
-  std::cout << dyf(1, 0) << ' ' << dyf(0, 1) << '\n';
+  int colwidth = 3;
+  for (double x = -5; x <= 5; x += 1) {
+    for (double y = -5; y <= 5; y += 1) {
+      std::cout << '(' //
+                << std::setw(colwidth) << std::right << dxf(x, y) << ", "
+                << std::setw(colwidth) << std::right << dyf(x, y) << ") ";
+    }
+    std::cout << '\n';
+  }
 }
