5798002b1935ee45579c069448d1d96871af222a
Semir Vrana <semir.vrana@gmx.de>
2020-01-27 13:03:59 +0100

    Small change

--- test.cc
+++ test.cc
@@ -10,8 +10,7 @@ int main() {
   constexpr auto x = ad::_0;
   constexpr auto y = ad::_1;
 
-  static_assert(
-      std::is_same_v<std::decay_t<decltype(123_c)>, ad::static_constant<123>>);
+  static_assert(same_type(123_c, ad::static_constant<123>{}));
 
   static_assert(same_type(x.derive(x), 1_c));
   static_assert(same_type(x.derive(y), 0_c));
