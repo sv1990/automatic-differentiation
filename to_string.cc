49cc2e7c464bb79701a59d4091e66e870d7dc403
Semir Vrana <semir.vrana@gmx.de>
2020-01-28 13:42:51 +0100

    Change to_string example

--- to_string.cc
+++ to_string.cc
@@ -7,6 +7,7 @@ int main() {
   using namespace ad::literals;
   const auto x = ad::_0;
   const auto f = (ad::sin(x) + 1_c) * (ad::pow(x, 2_c) - 1_c);
-  std::cout << ad::to_string(f) << '\n';
-  std::cout << ad::to_string(f.derive()) << '\n';
+  std::cout << "f (" << ad::to_string(x) << ") = " << ad::to_string(f) << '\n';
+  std::cout << "f'(" << ad::to_string(x) << ") = " << ad::to_string(f.derive())
+            << '\n';
 }
