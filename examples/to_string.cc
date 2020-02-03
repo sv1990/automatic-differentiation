7cbb775d882d5bcb4a6972e9f32e2a842f45ca83
Semir Vrana <semir.vrana@gmx.de>
2020-02-03 13:02:01 +0100

    Improve example

--- examples/to_string.cc
+++ examples/to_string.cc
@@ -10,11 +10,17 @@ struct format_variable<variable<0>> {
 };
 } // namespace ad
 
+namespace ad { namespace detail {
+template <typename E, std::enable_if_t<is_expression_v<E>>* = nullptr>
+std::ostream& operator<<(std::ostream& os, const E& e) {
+  return os << to_string(e);
+}
+}} // namespace ad::detail
+
 int main() {
   using namespace ad::literals;
   const auto x = ad::_0;
   const auto f = (-ad::sin(x) + 1_c) * -(ad::pow(x, 2_c) - 1_c);
-  std::cout << "f (" << ad::to_string(x) << ") = " << ad::to_string(f) << '\n';
-  std::cout << "f'(" << ad::to_string(x) << ") = " << ad::to_string(f.derive())
-            << '\n';
+  std::cout << "f (" << x << ") = " << f << '\n';
+  std::cout << "f'(" << x << ") = " << f.derive() << '\n';
 }
